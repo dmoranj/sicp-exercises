2b92de4b69804107d666f61070cb29833add8206
Daniel Moran <dmj@tid.es>
2017-02-05 12:18:00 +0100

    ADD Exercise 1.12

---
+++ src/sicp_exercises/lesson1.clj
@@ -0,0 +1,48 @@
+(ns sicp-exercises.lesson1)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+;; Exercise 1.11
+(defn f [n]
+  (if (< n 3)
+    n
+    (+ (f (dec n))
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
+
+(defn fiter[f1 f2 f3 count]
+  (if (= count 0)
+    f1
+    (fiter (+ f1 (* 2 f2) (* 3 f3)) f1 f2 (dec count))))
+
+
+(defn f-i [n]
+  (if (< n 3)
+    n
+    (fiter 2 1 0 (- n 2))))
+
+;; Exercise 1.12
+(defn pairwise-sum-iter[v acc counter]
+  (if (== 0 counter)
+    acc
+    (pairwise-sum-iter (rest v) (conj acc (+ (first v) (second v))) (dec counter))))
+
+(defn pairwise-sum [v]
+  (pairwise-sum-iter v [] (dec (count v))))
+
+(pairwise-sum [1 3 3 1])
+
+
+(defn pascal [n]
+  (cond
+    (== n 0) [1]
+    (== n 1) [1 1]
+    :else    (concat [1]
+                     (pairwise-sum (pascal (dec n)))
+                     [1])
+  ))
+
+
