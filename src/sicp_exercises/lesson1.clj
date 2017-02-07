02591fb22197e95761d1786881ad3eb111979828
Daniel Moran <dmj@tid.es>
2017-02-07 08:40:17 +0100

    ADD Exercise 1.21

--- src/sicp_exercises/lesson1.clj
+++ src/sicp_exercises/lesson1.clj
@@ -90,3 +90,16 @@
 (defn showfib[n]
   (map fib (range n)))
 
+;; Exercise 1.21
+(defn divides? [a b]
+  (== (rem b a) 0))
+
+(defn find-divisor [n test-divisor]
+  (cond (> (* test-divisor test-divisor) n) n
+        (divides? test-divisor n) test-divisor
+        :else (find-divisor n (+ test-divisor 1))))
+
+
+(defn smallest-divisor [n]
+  (find-divisor n 2))
+
