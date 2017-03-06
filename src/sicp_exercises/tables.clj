dd3a43dcd81b67962f8c94dce308adb55077696b
Daniel Moran <dmj@tid.es>
2017-03-06 17:48:08 +0100

    ADD Exercise 3.25 (Insertions)

--- src/sicp_exercises/tables.clj
+++ src/sicp_exercises/tables.clj
@@ -15,13 +15,6 @@
       (.getCdr record)
       false)))
 
-(defn t-insert! [k value table]
-  (let [record (t-assoc k (.getCdr table))]
-    (if record
-      (.setCdr record value)
-      (.setCdr table
-               (Pair. (Pair. k value) (.getCdr table))))))
-
 (defn lookup [ key1 key2 table ]
   (let [ subtable (t-assoc key1 (.getCdr table)) ]
     (if subtable
@@ -31,35 +24,41 @@
           false))
       false)))
 
-(defn t-insert2! [key1 key2 value table]
-  (let [ subtable (t-assoc key1 (.getCdr table)) ]
-    (if subtable
-      (let [ record (t-assoc key2 (.getCdr subtable)) ]
-        (if record
-          (.setCdr record value)
-          (.setCdr subtable
-                   (Pair. (Pair. key2 value)
-                          (.getCdr subtable)))))
+(defn rec-insert [k value table]
+  (let [record (t-assoc k (.getCdr table))]
+    (if record
+      (.setCdr record value)
       (.setCdr table
-               (Pair. (Pair. key1
-                             (Pair. (Pair. key2 value)
-                                    nil))
-                      (.getCdr table))))
-    'ok))
+               (Pair. (Pair. k value) (.getCdr table))))))
+
+(defn t-insert! [key-vector value table]
+  (let [ first-key (first key-vector)
+         rest-keys (rest key-vector)
+         subtable (t-assoc first-key (.getCdr table))]
+    (if subtable
+      (if (== (count rest-keys) 1)
+        (rec-insert (first rest-keys) value subtable)
+        (t-insert! rest-keys value subtable))
+
+      (let [ new-subtable (Pair. first-key nil) ]
+        (.setCdr table
+                 (Pair. new-subtable
+                        (.getCdr table)))
+        (t-insert! key-vector value table)))))
 
 (defn make-table []
   (Pair. '*table* nil))
 
 (defn show-tables-2-keys []
   (let [ table  (make-table) ]
-    (t-insert2! :math :+ 43 table)
-    (t-insert2! :math :- 45 table)
-     (t-insert2! :math :* 42 table)
-     (t-insert2! :letters :b 97 table)
-     (t-insert2! :letters :a 98 table)
+    (t-insert! [ :math :+ ] 43 table)
+    (t-insert! [ :math :- ] 45 table)
+     (t-insert! [ :math :* ] 42 table)
+     (t-insert! [ :letters :b ] 97 table)
+     (t-insert! [ :letters :a ] 98 table)
      (println "Full table: " table)
-     (println "Value for keys :math :* = " (lookup :math :* table))
-     (println "Value for key :letters :b = " (lookup :letters :b table))
+;;     (println "Value for keys :math :* = " (lookup :math :* table))
+;;     (println "Value for key :letters :b = " (lookup :letters :b table))
     ))
 
 (defn show-tables[]
@@ -70,3 +69,5 @@
     (println "Value for key :math = " (t-lookup :math table))
     (println "Value for key :letters = " (t-lookup :letters table))
     ))
+
+(show-tables-2-keys)
