d1c21c22ce0344be986b25deb8f02d464be95bca
Daniel Moran <dmj@tid.es>
2017-03-27 09:55:07 +0200

    ADD Exercise 3.56

--- src/sicp_exercises/lesson3.clj
+++ src/sicp_exercises/lesson3.clj
@@ -786,3 +786,18 @@
 ;; Exercise 3.55
 (def partial-sums (cons-stream 1 (add-streams (stream-cdr integers) partial-sums)))
 
+;; Exercise 3.56
+(defn merge-stream [s1 s2]
+  (cond
+    (stream-null? s1) s2
+    (stream-null? s2) s1
+    :else (let [ s1car (stream-car s1)
+                 s2car (stream-car s2) ]
+            (cond
+              (< s1car s2car) (cons-stream s1car (merge-stream (stream-cdr s1) s2))
+              (> s1car s2car) (cons-stream s2car (merge-stream s1 (stream-cdr s2)))
+              :else           (cons-stream s1car (merge-stream (stream-cdr s1)
+                                                        (stream-cdr s2)))))))
+
+(def S (cons-stream 1 (merge-stream (scale-stream S 2) (merge-stream (scale-stream S 3) (scale-stream S 5)))))
+
